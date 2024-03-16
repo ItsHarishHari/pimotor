d47d5199b8a63205c0e1db436e68f2f1653761ac
ItsHarishHari <114843867+ItsHarishHari@users.noreply.github.com>
2024-03-17 00:22:48 +0530

    Add files via upload

---
+++ from robot import Robot.py
@@ -0,0 +1,32 @@
+from robot import Robot
+from time import sleep
+
+robot = Robot()
+
+def forward():
+    robot.forward()
+
+def backward():
+    robot.backward()
+
+while True:
+    forward()
+    sleep(1)
+    backward()
+    sleep(1)
+from robot import Robot
+from time import sleep
+
+robot = Robot()
+
+def forward():
+    robot.forward()
+
+def backward():
+    robot.backward()
+
+while True:
+    forward()
+    sleep(1)
+    backward()
+    sleep(1)
